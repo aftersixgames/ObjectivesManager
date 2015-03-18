fda95c377ebdf9e48b1a1d4c220aa92096ea9626
Geison Biazus <geisonbiazus@gmail.com>
2015-03-17 22:12:19 -0300

    first commit

---
+++ Assets/Scripts/Model/Objective.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+[Serializable]
+public class Objective {
+
+	public string key;
+	public string description;
+	public float targetProgress;
+	public bool saveEveryProgress = false;
+
+	public float currentProgress;
+	public bool earned = false;
+
+	public bool Earned { 
+		get { return earned; } 
+	}
+
+	public float CurrentProgress {
+		get { return currentProgress; }
+	}
+
+	public string StoreKey {
+		get { return "objectives_manager:objective:" + key; }
+	}
+
+	public void AddProgress(float progress) {
+		withProgress(() => currentProgress += progress);
+	}
+
+	public void SetProgress(float progress) {
+		withProgress(() => currentProgress = progress);
+	}
+
+	public void Load() {
+		SetProgress(PlayerPrefs.GetFloat(StoreKey, 0.0f));
+	}
+
+	public void Save() {
+		PlayerPrefs.SetFloat(StoreKey, currentProgress);
+		PlayerPrefs.Save();
+	}
+
+	private void withProgress(Action action) {
+		if (earned) return;
+		action();
+		if (saveEveryProgress) Save();
+		Earn();
+	}
+
+	private void Earn() {
+		if (currentProgress >= targetProgress) {
+			earned = true;
+			currentProgress = targetProgress;
+			Save();
+		}
+	}
+}
---
+++ Assets/Scripts/Model/ObjectivesManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+[Serializable]
+public class ObjectivesManager {
+
+	public List<Objective> objectives;
+	public List<Objective> current;
+	public List<Objective> completed;
+
+
+}
