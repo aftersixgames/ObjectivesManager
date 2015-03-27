df937b6d113f01657c5a188ee95f3cfa067fee67
Geison Biazus <geisonbiazus@gmail.com>
2015-03-26 21:05:19 -0300

    objective gui

---
+++ Assets/Scripts/Controllers/ObjectivesController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectivesController : MonoBehaviour {
+
+	public ObjectivesManager objectivesManager;
+
+	// Use this for initialization
+	void Start () {
+		objectivesManager.Init();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/GUI/ObjectiveGUI.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class ObjectiveGUI : MonoBehaviour {
+
+	public Objective objective;
+	public Text description;
+	public Text progress;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	void Update () {
+		description.text = objective.description;
+		progress.text = objective.CurrentProgress + " / " + objective.targetProgress;
+	}
+}
